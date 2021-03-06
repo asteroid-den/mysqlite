333c14580e4f3a4353b8dc9c7f04fd93e3e512c7
asteroid_den <denbartolomee@gmail.com>
2021-03-06 09:12:00 +0200

    Dependencies autoinstall

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 import setuptools
 from mysqlite import __version__
 
-with open("README.md", "r", encoding = "utf-8") as fh:
-    long_description = fh.read()
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+with open("requirements.txt", encoding="utf-8") as f:
+    requires = f.read().split()
 
 setuptools.setup(
-    name = "mysqlite",
-    version = __version__,
-    author = "asteroid_den",
-    author_email = "denbartolomee@gmail.com",
-    description = "Minimalistic MySQL and SQLite 3 ORM",
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
-    url = "https://bitbucket.org/asteroid_den/mysqlite/",
+    name="mysqlite",
+    version=__version__,
+    author="asteroid_den",
+    author_email="denbartolomee@gmail.com",
+    description="Minimalistic MySQL and SQLite 3 ORM",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url = "https://github.com/asteroidden/mysqlite/",
     packages = ['mysqlite'],
     license = 'MIT',
     classifiers = [
@@ -20,5 +23,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires = requires,
     python_requires = '>=3.6',
 )
